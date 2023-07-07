b4080e5ccb4ec88fce6ccb835308807595dc25a4
Bogdan Bosca <bogdan-dan.bosca@endava.com>
2023-07-08 01:35:14 +0300

    feat: add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { type FC, StrictMode } from "react";
+import { Home } from "./pages";
 
 export const App: FC = () => {
-  return <StrictMode>hello world</StrictMode>;
+  return (
+    <StrictMode>
+      hello world
+      <Home />
+    </StrictMode>
+  );
 };
