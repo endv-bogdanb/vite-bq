06ade57f9c1af565607888f96a47f9183d39d03a
Bogdan Bosca <bogdan-dan.bosca@endava.com>
2023-07-08 03:49:47 +0300

    feat: add only used icons

--- plugins/copySvgPlugin.ts
+++ plugins/copySvgPlugin.ts
@@ -1,15 +1,28 @@
-import { existsSync } from "node:fs";
 import { cp } from "node:fs/promises";
 import { type PluginOption } from "vite";
 
 export const copySvgPlugin: () => PluginOption = () => ({
   buildStart: async () => {
-    if (existsSync("public/svg")) return Promise.resolve();
-
     console.log("Copy svg");
-    await cp("node_modules/@bee-q/core/dist/bee-q/svg", "public/svg", {
-      recursive: true,
-    });
+
+    const weight = ["thin", "light", "regular", "bold", "fill", "duotone"];
+
+    // NOTE: copy only svgs that are used in project
+    const svgs = ["package", "info", "x"];
+
+    await Promise.all(
+      svgs
+        .map((svg) => {
+          return weight.map((weight) => {
+            const ext = `${svg}${weight === "regular" ? "" : `-${weight}`}.svg`;
+            const src = `node_modules/@bee-q/core/dist/bee-q/svg/${weight}/${ext}`;
+            const dest = `public/svg/${weight}/${ext}`;
+            return cp(src, dest, { recursive: true });
+          });
+        })
+        .flat(),
+    );
+
     console.log("Copy svg end");
   },
   name: "copy-svg-plugin",
