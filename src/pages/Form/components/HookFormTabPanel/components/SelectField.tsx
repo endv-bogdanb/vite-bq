e27328252d488e5cbdff9cd6a50109383b85949f
Bogdan Bosca <109202804+endv-bogdanb@users.noreply.github.com>
2023-09-01 12:22:41 +0300

    feat: add select example (#10)
    * feat: add react-hook-form select example
    
    * feat: add formik select example

---
+++ src/pages/Form/components/HookFormTabPanel/components/SelectField.tsx
@@ -0,0 +1,59 @@
+import { type ReactNode } from "react";
+import {
+  type FieldPath,
+  type FieldValues,
+  useController,
+  type UseControllerProps,
+} from "react-hook-form";
+import { type JSX } from "@bee-q/core";
+import { BqSelect } from "@bee-q/react";
+
+export interface SelectFieldProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+> extends UseControllerProps<TFieldValues, TName>,
+    Omit<JSX.BqSelect, "name"> {
+  children: ReactNode;
+  label: string;
+}
+
+export const SelectField = <
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+>({
+  children,
+  control,
+  defaultValue,
+  label,
+  name,
+  rules,
+  ...props
+}: SelectFieldProps<TFieldValues, TName>) => {
+  const { field, fieldState } = useController({
+    control,
+    defaultValue,
+    name,
+    rules,
+  });
+
+  return (
+    <BqSelect
+      ref={field.ref}
+      name={field.name}
+      value={field.value}
+      onBqSelect={field.onChange}
+      onBqBlur={field.onBlur}
+      onBqClear={field.onChange}
+      {...props}
+      validationStatus={fieldState.invalid ? "error" : "none"}
+    >
+      <label slot="label">{label}</label>
+      {fieldState.error ? (
+        <span slot="helper-text">
+          {fieldState.error.message || fieldState.error.type}
+        </span>
+      ) : null}
+      {children}
+    </BqSelect>
+  );
+};
