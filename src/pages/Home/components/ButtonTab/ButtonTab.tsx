8945cf2912c426276805d026fb9f40b222584497
Bogdan Bosca <109202804+endv-bogdanb@users.noreply.github.com>
2024-04-10 15:35:13 +0300

    feat: add emotion styled components example (#18)

--- src/pages/Home/components/ButtonTab/ButtonTab.tsx
+++ src/pages/Home/components/ButtonTab/ButtonTab.tsx
@@ -1,12 +1,29 @@
 import { type FC } from "react";
 import { BqButton } from "@beeq/react";
+import styled from "@emotion/styled";
 import classes from "./ButtonTab.module.css";
+
+const StyledBqButton = styled(BqButton)({
+  "&::part(button)": {
+    "&:active": {
+      backgroundColor: "var(--bq-corai-500)",
+    },
+    "&:hover": {
+      backgroundColor: "var(--bq-corai-600)",
+    },
+    backgroundColor: "var(--bq-corai-500)",
+    textTransform: "capitalize",
+  },
+  "--bq-button--medium-paddingX": "var(--bq-spacing-xl)",
+  "--bq-button--medium-paddingY": "var(--bq-spacing-xl)",
+});
 
 export const ButtonTab: FC = () => {
   return (
     <div className={classes.wrapper}>
       <BqButton>default button</BqButton>
       <BqButton className={classes.custom}>custom button</BqButton>
+      <StyledBqButton>styled custom button</StyledBqButton>
     </div>
   );
 };
